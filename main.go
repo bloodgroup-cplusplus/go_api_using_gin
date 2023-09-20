926c6acd1ce7767d392825b15c72a95ad7597dde
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2023-09-20 14:22:57 +0530

    dynamic routes creation

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -24,6 +25,37 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+//get book by id
+
+func getBookById(id string) (*book, error) {
+	// if the id doesn't exist return error
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+			// so that we can modify attributes
+		}
+	}
+	return nil, errors.New("Book not found")
+}
+
+func bookById(c *gin.Context) {
+	// get book by id
+	id := c.Param("id")
+	book, err := getBookById(id)
+
+	// if we do have an error (retun error)
+	// return a custom response saying bad request or bad response
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		// gin.H is a custom json writing element
+		return
+
+	}
+
+	c.IndentedJSON(http.StatusOK, book)
+}
+
 func createBook(c *gin.Context) {
 	var newBook book
 	// from context bind the data payload from the content
@@ -41,6 +73,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
 	router.POST("/books", createBook)
+	router.GET("/books/:id", bookById) // set up a path parameter
 	router.Run("localhost:8080")
 
 }
